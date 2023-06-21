bd0e540f1f2c104650124b6a630be1f58dbe701d
Blair <bleava023@gmail.com>
2023-06-20 18:29:42 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,6 @@
 /**
  * main - Entry point
  *
- * Description: Prints "_putchar" followed by a new line
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -18,19 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
-}
-
-/**
- * _putchar - Writes a character to the standard output (stdout)
- *
- * @c: The character to be written
- *
- * Return: On success, returns the character written.
- * On error, returns -1 and sets errno.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
